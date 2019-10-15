9b4dadb570c12a9dd9e9f5f152604b02638dbcca
RoniUdacity <roni@dinatechnologies.com>
2019-10-15 11:59:17 -0400

    Setup Student Location Object
    RR: 10-15-2019
    Setup objects;
    1. Student location
    2. Logged in user info
    3. Location coordinates

---
+++ OnTheMapRoniR/OnTheMapRoniR/Model/Data/LocationCoordinatesModel.swift
@@ -0,0 +1,15 @@
+//
+//  LocationCoordinatesModel.swift
+//  OnTheMapRoniR
+//
+//  Created by Roni Rozenblat on 9/27/19.
+//  Copyright © 2019 dinatech. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct  LocationCoordinatesModel{
+    let longitudeVal: CLLocationDegrees
+    let latitudeVal: CLLocationDegrees
+}
---
+++ OnTheMapRoniR/OnTheMapRoniR/Model/Data/LoggedInUserInfo.swift
@@ -0,0 +1,15 @@
+//
+//  LoggedInUserInfo.swift
+//  OnTheMapRoniR
+//
+//  Created by Roni Rozenblat on 9/30/19.
+//  Copyright © 2019 dinatech. All rights reserved.
+//
+
+import Foundation
+
+struct LoggedInUserInfo {
+    let uniqueKey:String
+    let firstName:String
+    let lastName:String
+}
