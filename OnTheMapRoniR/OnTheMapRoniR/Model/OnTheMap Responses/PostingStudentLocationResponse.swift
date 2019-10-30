9fb98759a9022ea766384144b53dc84215ea89cc
RoniUdacity <roni@dinatechnologies.com>
2019-10-30 10:24:23 -0400

    Binary file has changed
    RR: 10-30-2019
    Binary file has changed.

---
+++ OnTheMapRoniR/OnTheMapRoniR/Model/OnTheMap Responses/PostingStudentLocationResponse.swift
@@ -0,0 +1,14 @@
+//
+//  PostingStudentLocationResponse.swift
+//  OnTheMapRoniR
+//
+//  Created by Roni Rozenblat on 10/23/19.
+//  Copyright © 2019 dinatech. All rights reserved.
+//
+
+import Foundation
+
+struct PostingStudentLocationResponse: Codable {
+    let createdAt: String
+    let objectId: String
+}
